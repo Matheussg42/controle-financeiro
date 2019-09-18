b2feee211dadd8a47634ca1c0d40180cb8574a65
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2019-09-17 22:37:46 -0300

    Create API component

--- frontend/src/components/Api.js
+++ frontend/src/components/Api.js
@@ -1,3 +1,20 @@
-const urlAPI = "http://127.0.0.1:8000/api/v1" 
+import React, { Component } from "react";
+import { Redirect } from 'react-router'
 
-export default urlAPI
+class Api extends Component {
+
+    static urlAPI = "http://127.0.0.1:8000/api/v1"
+  
+    static config = {
+        headers: {
+           Authorization: "bearer " + localStorage.getItem('app-token')
+        }
+    }
+
+    static apiExpired() {
+        localStorage.removeItem('app-token');
+        return <Redirect to='/login' />
+    }
+}
+
+export { Api };
